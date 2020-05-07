ab496acb9d856aa276bc4a12e244a31c48af8366
Jeremy Thaller <jeremy@thallerfamily.com>
2020-05-06 20:36:03 -0400

    improved code commenting

--- friend_classifier.py
+++ friend_classifier.py
@@ -1,13 +1,15 @@
-# Starting code was stuff I wrote for codeacademy course where they build a friends_classifier
+#Jeremy Thaller May 6, 2020
+# Given an unseen message, the script predicts who sent the message.
+# I used personal fb chats as training data, and I used messages from group chats as testing data  
+# The only thing needed to be edited is the mystery_message string.
 
 #Import docs with messages in them
-
+from extract_messages import messages_dict
 
 # import sklearn modules here:
 # from preprocessing import preprocess_text
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from extract_messages import messages_dict
 
 rohan = "Rohan Kadambi"
 mike = "Zeran Ji"
@@ -16,13 +18,12 @@ jeremy = "Jeremy Thaller"
 
 # Setting up the combined list of friends' writing samples
 friends_docs = messages_dict[rohan]+ messages_dict[mike] + messages_dict[thomas] + messages_dict[jeremy]
-# Setting up labels for your three friends
+# Setting up labels for the 4 friends
 friends_labels = [1]*(len(messages_dict[rohan])) + [2]*(len(messages_dict[mike])) + [3]*(len(messages_dict[thomas])) + [4]*(len(messages_dict[jeremy]))
 
+#never before seen message for which the NN will predict the sender
+mystery_message = "big boi manipulation lol"
 
-mystery_message = """
-big boi manipulation lol
-"""
 
 # Create bow_vectorizer:
 bow_vectorizer = CountVectorizer()
@@ -37,7 +38,7 @@ friends_classifier = MultinomialNB()
 # Train the classifier:
 friends_classifier.fit(friends_vectors, friends_labels)
 
-# Change predictions:
+# Change prediction back to a name:
 predictions = friends_classifier.predict(mystery_vector)
 if predictions == [1]:
     predictions = rohan
@@ -47,7 +48,8 @@ elif predictions == [3]:
     predictions = thomas_json1
 elif predictions == [4]:
     predictions = jeremy
-print(f"prediction: message from {predictions}")
+print(f"Test message for prediction: \"{mystery_message}\"")
+print(f"Prediction: Message sent from {predictions}")
 
 
 # # Uncomment the print statement:
